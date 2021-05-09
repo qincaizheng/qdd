1f1f52bc6258571aade1d2040b153e0e65d83320
qincaizheng <dhdxqcz@gmail.com>
2021-05-09 16:43:23 +0800

    创建 login.php

---
+++ php/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+$username=$_POST['username'];
+$password=$_POST['password'];
+if ($username=='小乔'&$password=='小乔') {
+	
+}elseif ($username=='qdd'&$password=='qdd') {
+	echo "<script> window.open('./kaifa.html','_self') </script>";
+}else{
+
+echo "未开发";
+}
+
+?>
+</body>
+
+</html>
+
